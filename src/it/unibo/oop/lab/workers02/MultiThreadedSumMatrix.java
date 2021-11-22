f72020df83b579e22429b2c32d3a9976b947e286
status <marco.costantini7@studio.unibo.it>
2021-11-22 18:23:28 +0100

    Create class MultiThreadedSumMatrix

---
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.lab.workers02;
+
+public class MultiThreadedSumMatrix implements SumMatrix{
+
+    @Override
+    public double sum(double[][] matrix) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
